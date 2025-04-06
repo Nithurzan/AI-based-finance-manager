f89965dd37ae02fe6b6f7d47c00a1261c3f4b0de
Nithurzan <yogalingam.nithurzan@gmail.com>
2025-04-06 08:44:03 +0530

    transaction model update

--- backend/transaction/models.py
+++ backend/transaction/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Optional
 from enum import Enum
@@ -13,7 +13,12 @@ class Transaction(BaseModel):
     category: str = None
     type: Type
     description:str
-    date : datetime = datetime.now()
+    date: datetime = Field(default_factory=datetime.now)
+
+    class Config:
+        json_encoders = {
+            datetime: lambda v: v.strftime('%Y-%m-%d')  # <-- Format here
+        }
 
 class TransactionInDB(Transaction):
     user_id: str 
