2dcab8878a9b3ebfdd39524b8582777f1124f84c
Nithurzan <yogalingam.nithurzan@gmail.com>
2025-04-28 16:55:56 +0530

    update

--- backend/authentication/auth_route.py
+++ backend/authentication/auth_route.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException,status,APIRouter
-from models import User,UserInDB
+from .schema import User
+from .auth import password_hash
+from Backend.database import get_db 
 
 
 router = APIRouter()
@@ -8,14 +10,19 @@ router = APIRouter()
 @router.post("/regiester")
 async def register_user(user:User):
     try:
+        db = await get_db() 
         user_dict = user.dict()
-        # existing_user = await db.user.find_one({})
+
+        existing_user = await db.users.find_one({"email": user_dict["email"]})
+        if existing_user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+
         user_dict["hashed_pssword"] = password_hash(user.password)
         del user_dict['password']
 
-        # result = await db.users.insert_one(user_dict)
-        # if not result.inserted_id:
-            # raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="User regiestration fail")
+        result = await db.users.insert_one(user_dict)
+        if not result.inserted_id:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="User regiestration fail")
         return {"message":"User regiestered successfully"}
     
     except Exception as e:
